3630883adcd6198f9f91781a9399f0ba18d9a0b8
Neyrision <neyrision@gmail.com>
2019-11-14 20:55:06 +0100

    added graphic objects

---
+++ PseudoEngine/Graphics/Polygon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PseudoEngine.Graphics
+{
+    class Polygon
+    {
+        public Vertice verticeX;
+        public Vertice verticeY;
+        public Vertice verticeZ;
+        public Polygon(Vertice verticeX, Vertice verticeY, Vertice verticeZ )
+        {
+            this.verticeX = verticeX;
+            this.verticeY = verticeY;
+            this.verticeZ = verticeZ;
+        }
+    }
+}
---
+++ PseudoEngine/Graphics/Vertice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PseudoEngine.Graphics
+{
+    class Vertice
+    {
+        public double x { get; private set; }
+        public double y { get; private set; }
+        public double z { get; private set; }
+        public Vertice(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
